71d0d5ab9d31968635d47f3243f50abedf473164
Anshul <anshulkumar3552@gmail.com>
2022-06-02 00:34:50 +0530

    commit5

--- app.js
+++ app.js
@@ -395,7 +395,7 @@ app.get("/BS_contact", function(req, res){
 })
 
 let port = process.env.PORT;
-if(port==NULL || port==""){
+if(port==null || port==""){
     port=3000
 }
 app.listen(port ,function(){
